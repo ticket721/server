65cdb514feb938a94a68e75bba29997f6b809868
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2019-02-23 20:05:25 -0500

    [+] Setup basic entities with strapi
      - Generated action entity
      - Generated address entity
      - Generated event entity
      - Generated ticket entity
    
    SRV #3

--- tasks/setup.js
+++ tasks/setup.js
@@ -7,12 +7,15 @@ const ts = require('gulp-typescript');
 const tsConfig = require(from_current('./tsconfig.json'));
 
 module.exports.tsc_hooks = async function tsc_hooks() {
+    const save_dir  = process.cwd();
+    process.chdir(from_current('.'));
     signale.info('Transpiling hooks');
     return gulp.src([
         from_current('./hooks_sources') + '/**/*.ts',
         '!' + from_current('./hooks_sources') + '/**/*.test.ts'])
         .pipe(ts(tsConfig.compilerOptions))
-        .pipe(gulp.dest(from_current('./hooks')));
+        .pipe(gulp.dest(from_current('./hooks')))
+        .on('end', () => {process.chdir(save_dir)})
 };
 
 module.exports.postgres = async function postgres() {
