c8080a1b35787832144a8aa5bd0d6519d9f45f4a
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2019-03-01 12:53:58 -0500

    [+] Setup server tasks
      - server:setup => start required containers
      - server:start => starts strapi
      - server:clean => clean everything
    
    SRV #3

--- tasks/clean.js
+++ tasks/clean.js
@@ -1,4 +1,5 @@
 const {postgres_development_clean} = require('./postgres/development');
+const {eth_node_development_clean} = require('./eth_node/development');
 const {from_current} = require('./misc');
 const rimraf = require('rimraf');
 
@@ -6,9 +7,11 @@ module.exports.remove_hooks = async function remove_hooks() {
     rimraf.sync(from_current('./hooks'));
 };
 
-module.exports.postgres_clean = async function postgres_clean() {
+module.exports.clean = async function clean() {
     switch (process.env.T721_SERVER) {
         case 'development':
-            return postgres_development_clean();
+            await postgres_development_clean();
+            await eth_node_development_clean();
+            return ;
     }
 };
