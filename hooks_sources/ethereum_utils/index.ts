b39581a9caffc02f8160894706206897fe1966f2
Iulian Rotaru <iulian@rotaru.fr>
2019-06-26 12:07:22 +0200

    feat: ropsten ci/cd setup (#18)
    * feat: build + publish docker images
    
    * fix: height bridge to synchronization in modules
    
    * fix: integrate wait and poll into module startups
    
    * fix: add public node characs
    
    Was inserting cluster ip into database, new overrides make it insert the
    public informations of the geth node
    
    * fix: add starting block in antenna
    
    * chore: update develop (#17)
    
    * chore: update staging (#15)
    
    * feat: build + publish docker images
    
    * fix: height bridge to synchronization in modules
    
    * fix: integrate wait and poll into module startups
    
    * fix: add public node characs
    
    Was inserting cluster ip into database, new overrides make it insert the
    public informations of the geth node
    
    * fix: add starting block in antenna
    
    * feat: support for mint currency / price
    
    * feat: add action timestamp based on block.
    
    * fix: server's web3 and module fix
    
      - Add ENV variables to connect the server properly to the eth node
      - Remove console logs from modules
      - Remove unused imports from modules
    
    * fix: add wait loop on queued event fetch
    
      - Cluster node might not be as up to date as an infura node
      - Giving him more time to discover the given transaction

--- hooks_sources/ethereum_utils/index.ts
+++ hooks_sources/ethereum_utils/index.ts
@@ -1,8 +1,4 @@
-import { Portalize } from 'portalize';
-import * as path from 'path';
 import Web3 = require('web3');
-
-const from_current = (add_path: string): string => path.join(path.resolve(path.join(__dirname, '../..')), add_path);
 
 export = (strapi: StrapiCtx): Hook => {
 
@@ -13,21 +9,9 @@ export = (strapi: StrapiCtx): Hook => {
         },
 
         initialize: async (cb: (error?: Error) => void): Promise<void> => {
-            Portalize.get.setPortal(from_current('./portal'));
-            Portalize.get.setModuleName('server');
-
-            if (!Portalize.get.requires({
-                file: 'event_infos.json',
-                from: 'contracts',
-                action: 'add'
-            })) {
-                return cb(new Error('Cannot find event informations inside the portal'));
-            }
-
-            const network_config = Portalize.get.get('network.json', {module: 'network'});
 
             // @ts-ignore
-            const web3 = new Web3(new Web3.providers.HttpProvider(`http://${network_config.host}:${network_config.port}`));
+            const web3 = new Web3(new Web3.providers.HttpProvider(`${process.env.ETH_NODE_PROTOCOL}://${process.env.ETH_NODE_HOST}:${process.env.ETH_NODE_PORT}`));
 
             strapi.ethereum = {
                 web3
